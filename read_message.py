32bc7616a92fa0f9b27ea217e0a33b216706f2e7
AlexWoIf <alexey.barannikov@gmail.com>
2024-06-05 14:37:50 +0300

    Add retry on gaierrors

--- read_message.py
+++ read_message.py
@@ -1,17 +1,28 @@
 import asyncio
+import datetime
+import socket
 
 import aiofiles
+
 
 async def write_to_file(file_path, content):
     async with aiofiles.open(file_path, mode='a') as f:
         await f.write(content)
 
 async def read_msg():
+    retry = 0
     while True:
-        reader, _ = await asyncio.open_connection('minechat.dvmn.org', 5000)
-        received_msg = await reader.readline()
-        await write_to_file('chat.log', received_msg.decode())
-        print(received_msg.decode().strip())
+        try:
+            reader, _ = await asyncio.open_connection('minechat.dvmn.org', 5000)
+            received_msg = await reader.readline()
+            timestamp = datetime.datetime.now().strftime("[%Y-%m-%d %H:%M]")
+            await write_to_file('chat.log', f'{timestamp}\t{received_msg.decode()}')
+            print(received_msg.decode().strip())
+            retry = 0
+        except socket.gaierror as exc:
+            print(f'Sleeping {retry}sec(s)')
+            await asyncio.sleep(retry)
+            retry = (retry + 1) * 2
 
 
 if __name__ == '__main__':
