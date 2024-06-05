0392238c927509a21ef031ee26c0b8ebb126af69
AlexWoIf <alexey.barannikov@gmail.com>
2024-06-05 12:57:09 +0300

    Save all messages to file

--- read_message.py
+++ read_message.py
@@ -1,10 +1,16 @@
 import asyncio
 
+import aiofiles
+
+async def write_to_file(file_path, content):
+    async with aiofiles.open(file_path, mode='a') as f:
+        await f.write(content)
 
 async def read_msg():
     while True:
         reader, _ = await asyncio.open_connection('minechat.dvmn.org', 5000)
         received_msg = await reader.readline()
+        await write_to_file('chat.log', received_msg.decode())
         print(received_msg.decode().strip())
 
 
